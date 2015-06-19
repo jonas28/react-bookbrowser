e33efc0a80495a8fb550bfcc3322260461232159
jonas28 <jonas@steps.fm>
2015-06-19 15:51:52 +0200

    implementet card to ResultScreen.js

--- ResultsScreen.js
+++ ResultsScreen.js
@@ -1,5 +1,7 @@
 'use strict';
 var React = require('react-native');
+var Dimensions = require('Dimensions');
+var windowSize = Dimensions.get('window');
 
 var {
     View,
@@ -8,6 +10,12 @@ var {
     Image,
     TouchableHighlight,
     StyleSheet,
+    AppRegistry,
+    ActivityIndicatorIOS,
+    Navigator,
+    TouchableOpacity,
+    Animation
+
 } = React;
 
 var BookDetails = require('./BookDetails');
@@ -22,27 +30,82 @@ var buildUrl = function(q) {
 var ResultsScreen = React.createClass({
     getInitialState: function() {
         return {
+            x: 0,
+            y: 0,
+            lastDragDirectio: 'Drag and Release',
             isLoading: true,
             dataSource: new ListView.DataSource({
                 rowHasChanged: (row1, row2) => row1 !== row2,
-        }),
-    };
-},
-componentDidMount: function() {
-    this.fetchResults(this.props.searchPhrase);
-},
+            }),
+        };
+    },
+    componentDidMount: function() {
+        this.fetchResults(this.props.searchPhrase);
+    },
 
-fetchResults: function(searchPhrase) {
-    fetch(buildUrl(searchPhrase))
-        .then(response => response.json())
-        .then(jsonData => {
-            this.setState({
-                isLoading: false,
-                dataSource: this.state.dataSource.cloneWithRows(jsonData.items)
-            });
-        console.dir(jsonData.items);
+    fetchResults: function(searchPhrase) {
+        fetch(buildUrl(searchPhrase))
+            .then(response => response.json())
+            .then(jsonData => {
+                this.setState({
+                    isLoading: false,
+                    dataSource: this.state.dataSource.cloneWithRows(jsonData.items)
+                });
+            console.dir(jsonData.items);
+            })
+            .catch(error => console.dir(error));
+    },
+
+    setPosition: function(e) {
+        this.setState({
+            x: this.state.x + (e.nativeEvent.pageX - this.drag.x),
+            y: this.state.y + (e.nativeEvent.pageY - this.drag.y)
+        });
+        this.drag.x = e.nativeEvent.pageX;
+        this.drag.y = e.nativeEvent.pageY;
+    },
+    resetPosition: function(e) {
+        this.dragging = false;
+        var left = e.nativeEvent.pageX < (windowSize.width/2),
+            displayText = left ? 'Released left' : 'Released right';
+
+        this.setState({
+            x: 0,
+            y: 0,
+            lastDragDirectio: displayText
         })
-        .catch(error => console.dir(error));
+    },
+    getRotationDegree: function(rotateTop, x) {
+        var rotation = ( (x/windowSize.width) * 100)/3;
+
+        var rotate = rotateTop ? 1 : -1,
+            rotateString = (rotation * rotate) + 'deg';
+
+        return rotateString;
+    },
+    getCardStyle: function() {
+        var transform = [{translateX: this.state.x}, {translateY: this.state.y}];
+
+        if (this.dragging) {
+            transform.push({rotate: this.getRotationDegree(this.rotateTop, this.state.x)})
+        }
+
+        return {transform: transform};
+    },
+    _onStartShouldSetResponder: function(e) {
+        this.dragging = true;
+
+        this.rotateTop = e.nativeEvent.locationY <= 150;
+
+        this.drag = {
+            x: e.nativeEvent.pageX,
+            y: e.nativeEvent.pageY
+        }
+
+        return true;
+    },
+    _onMoveShouldSetResponder: function(e) {
+        return true;
     },
 
     render: function() {
@@ -68,28 +131,24 @@ fetchResults: function(searchPhrase) {
             <ListView
                 dataSource={this.state.dataSource}
                 renderRow={this.renderBook}
-                style={styles.listView}
             />
         );
     },
 
     renderBook: function(book) { return (
-        <TouchableHighlight onPress={() => this.showBookDetails(book)}>
-            <View style={styles.row}>
-                <Image
-                    style={styles.thumbnail}
-                    source={{uri: book.volumeInfo.imageLinks.smallThumbnail}}
-                />
-                <View style={styles.rightContainer}>
-                    <Text style={styles.title}>
-                    {book.volumeInfo.title}
-                    </Text>
-                    <Text style={styles.subtitle}>
-                    {book.volumeInfo.subtitle}
-                    </Text>
-                </View>
+        <View style={styles.container}>
+            <View
+                onResponderMove={this.setPosition}
+                onResponderRelease={this.resetPosition}
+                onStartShouldSetResponder={this._onStartShouldSetResponder}
+                onMoveShouldSetResponder={this._onMoveShouldSetResponder}
+                style={[styles.card, this.getCardStyle()]}
+            >
+                <Text>
+                    Test
+                </Text>
             </View>
-        </TouchableHighlight>
+        </View>
         );
     },
 
@@ -104,47 +163,36 @@ fetchResults: function(searchPhrase) {
 });
 
 var styles = StyleSheet.create({
-
     container: {
         flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#5AC8FA',
+        justifyContent: 'center'
     },
-    label: {
-        fontSize: 24,
-        fontWeight: 'normal',
-        color: '#fff',
+    dragText: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0
     },
-    listView: {
+    card: {
+        borderWidth: 3,
+        borderRadius: 3,
+        borderColor: '#000',
+        width: 300,
+        height: 300,
+        padding: 10
     },
-    rightContainer: {
-        flex: 1,
+    cardImage: {
+        height: 260,
     },
-    row: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#5AC8FA',
-        paddingRight: 20,
-        marginTop: 1,
+    textLeft: {
+        position: 'absolute',
+        left:0,
+        top:0
     },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: '#fff',
-    },
-    subtitle: {
-        fontSize: 16,
-        fontWeight: 'normal',
-        color: '#fff',
-    },
-    thumbnail: {
-        width: 70,
-        height: 108,
-        marginRight: 16,
+    textRight: {
+        position: 'absolute',
+        right: 0,
+        top: 0
     }
 });
 
